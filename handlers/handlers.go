1cf766e4d483c55a296ce1cf59f54a332e7a938c
lalithbseervi <lcoding20@gmail.com>
2025-07-27 00:53:44 +0530

    hotfix: cors issue in prod environment

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,7 +6,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "https://eventloop-testbed.vercel.app/")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
